83d9df9f426ee66730f26b7b9b170901b3c62b57
Tiffany Eulalio <tyeulalio@gmail.com>
2024-04-19 17:04:49 -0700

    adding the 05_qtl_analysis scripts

---
+++ scripts/05_qtl_analysis/09_remove_outliers.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# using plink on scg
+module load plink2
+
+CHROMDIR=""
+
+# run plink to generate files 
+plink2 \
+    --make-bed \
+    --output-chr chrM \
+    --pfile "../../output/05_qtl_analysis/06_merge_bims/${CHROMDIR}all_chroms_hg38" \
+    --remove "../../output/05_qtl_analysis/08_format_eigenstrat/${CHROMDIR}population_outliers_hg38.txt" \
+    --out "../../output/05_qtl_analysis/09_remove_outliers/${CHROMDIR}all_chroms_hg38" 
+    
